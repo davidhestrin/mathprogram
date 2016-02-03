cd50871f2a313a74b2330675f09853a31d031706
David Hestrin <david.hestrin@gmail.com>
2016-02-02 17:44:30 -0800

    send multi e-mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def test_results_email(user, quiz_result)
     @user = user
     @quiz_result = quiz_result
-    mail(to: @user.email, subject: "Your test results")
+    mail(to: @user.email, fullbellyfitness@gmail.com, subject: "Your test results")
   end
 end
